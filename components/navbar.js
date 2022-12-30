658ab1013767d73a49da12e9a8333af6630ec07d
Jason <bodmand1@gmail.com>
2022-12-30 18:54:35 -0500

    update navbar responsive styles

--- components/navbar.js
+++ components/navbar.js
@@ -5,19 +5,18 @@ import Image from 'next/image'
 import styles from '../styles/navbar.module.css'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown, faSquarePlus } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = ({logo, navigation}) => {
     const [menuVis, setMenuVis] = React.useState('none');
     function mouseEnter() {
-        console.log('CLICKED!')
         setMenuVis('block')
     }
 
     function mouseLeave() {
-        console.log('Exit')
         setMenuVis('none')
     }
+    
     return (
         
         <nav className={styles.fullNav}>
@@ -28,35 +27,40 @@ const Navbar = ({logo, navigation}) => {
             </div>
 
             <div className={styles.navMenu}>
-                <ul className={styles.navLinks}>
-                    <li className={styles.navItem}>
-                        <Link href="/about">
-                            Our Team
-                        </Link>
-                    </li>
-                    <li className={styles.navItemParent} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
-                        <div className={styles.subMenu} >
-                            <Link href="/services">Services</Link>&nbsp;<span onMouseClick={mouseEnter}><FontAwesomeIcon className={styles.blueArrow} icon={faCaretDown} /></span>
-                            <div className={styles.dropdown} style={{display: `${menuVis ? menuVis : 'none' }`}} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
-                                <div className={styles.navItemChildList}>
-                                {navigation.map((item, key) => (
-                                    <div className={styles.navItemChild} key={key}>
-                                        <Link href={`/services/${item.slug.current}`}>
-                                            <span style={{whiteSpace: 'nowrap'}}>{item.title}</span>
-                                        </Link>
-                                    </div>
-                                    ))}
+                <div id={styles.menuToggle}>
+                    <input type="checkbox" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+
+                    <ul className={styles.navLinks}>
+                        <li className={styles.navItem}>
+                            <Link href="/about">
+                                Our Team
+                            </Link>
+                        </li>
+                        <li className={styles.navItemParent} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+                            <FontAwesomeIcon className={styles.bluePlus} icon={faSquarePlus} /><Link href="/services">Services</Link>&nbsp;<FontAwesomeIcon className={styles.blueArrow} icon={faCaretDown} />
+                                <div className={styles.dropdown} style={{display: `${menuVis ? menuVis : 'none' }`}} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+                                    <ul className={styles.navItemChildList}>
+                                    {navigation.map((item, key) => (
+                                        <li className={styles.navItemChild} key={key}>
+                                            <Link href={`/services/${item.slug.current}`}>
+                                                <p style={{whiteSpace: 'nowrap',}}>{item.title}</p>
+                                            </Link>
+                                        </li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            </div>
-                        </div>
-                        
-                    </li>
-                    <li className={styles.navItem}>
-                        <Link href="/contact">
-                            Contact Us
-                        </Link>
-                    </li>
-                </ul>
+                            
+                        </li>
+                        <li className={styles.navItem}>
+                            <Link href="/contact">
+                                Contact Us
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
            
         </nav>
