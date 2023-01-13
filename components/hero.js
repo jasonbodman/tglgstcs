1250da74564673425be3b01c3de5570bb510dd98
Jason <bodmand1@gmail.com>
2023-01-13 14:00:03 -0500

    update hero image properties

--- components/hero.js
+++ components/hero.js
@@ -5,6 +5,12 @@ import styles from '../styles/Home.module.css'
 const Hero = () => {
     return (
       <div className={styles.heroWrapper}>
+        <Image 
+          className={styles.heroBgImage} 
+          src="/images/wolf_hero_3600_1450.jpg" 
+          fill 
+          priority 
+/>
         <div className={styles.heroRight}>
           <div className={styles.heroLogoContainer}>
             <Image className={styles.heroLogo} src="/images/TOGOLOGISTICS.png" fill />
